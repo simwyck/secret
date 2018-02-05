618216591283eb40a4655b849c1d15dcf0b3f279
Simon Wyckaert <simon@digitold.com>
2018-02-05 17:34:50 +0100

    add pwd confirmation field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180205111528) do
+ActiveRecord::Schema.define(version: 20180205163409) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20180205111528) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "password_confirmation"
   end
 
 end
